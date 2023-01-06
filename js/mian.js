b788784af24df52c81247de174016577a01eccc0
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-01-06 11:09:15 +0100

    new changes

--- js/mian.js
+++ js/mian.js
@@ -100,3 +100,11 @@ darkModeButton.addEventListener('click', () => {
 // 
 // })
 
+
+grecaptcha.ready(function() {
+    grecaptcha.execute('6LfA69YjAAAAABRkLoCxfl_r0JeVMTBD4WDjSnag', {action: 'homepage'})
+    .then(function(token) {
+      document.getElementById('captchaResponse').value = token;
+      console.log( document.getElementById('captchaResponse').value = token);
+    });
+  });
