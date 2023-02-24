ab46b2429b7229caddda3aa9db761810b7d27c9a
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-02-24 11:33:07 +0100

    changes

--- js/swiper.js
+++ js/swiper.js
@@ -13,3 +13,18 @@ const swiper = new Swiper('.swiper-container', {
     },
   });
  
+
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '10px',
+    duration: 3000,
+    delay: 250,
+    //reset: true
+  })
+  
+  
+  
+    
+  sr.reveal(`.header_title`, { origin: 'left', distance: '100px',} )
+  sr.reveal(`.header_main-text-container`, { origin: 'right', distance: '100px',} )
+  sr.reveal(`.header_main-services-box-h4`, {duration: 2500, origin: 'top', distance: '15px',} )
