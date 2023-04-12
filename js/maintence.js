e7b56a3715e6f30a4289711f1fea1f20d5f3b17a
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-04-12 14:25:36 +0200

    develop

---
+++ js/maintence.js
@@ -0,0 +1,20 @@
+const buttonContainer = document.querySelector('.subscribe-btn__container');
+const buttonEfect = document.querySelectorAll('.btns');
+const activeTextContainer = document.querySelectorAll('.subscribe-container__text');
+
+
+buttonContainer.addEventListener('click', (e)=> {
+
+  const click = e.target.closest('button');
+
+  if (!click) return;
+
+  buttonEfect.forEach(remove => remove.classList.remove('button-effect-active') )
+  activeTextContainer.forEach(r => r.classList.remove('subscribe-active') )
+
+  click.classList.add('button-effect-active')
+
+  const text = document.querySelector(`.ctr-${click.dataset.tab}`)
+  text.classList.add('subscribe-active');
+
+})
